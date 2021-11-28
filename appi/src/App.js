b654fdf61ccdf52748135882550157e9cff42c27
Matias <matias.haapasalmi@live.com>
2021-11-28 18:00:04 +0200

    Ohjelma hakee nyt bitcoinin tamanhetkisen arvon

--- appi/src/App.js
+++ appi/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+const axios = require("axios").default;
+
+let url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur";
+
 
 function App() {
+
+  const [state, setState] = useState({})
+  
+  useEffect(()=>{
+    axios.get(url).then(res => {
+
+      setState(res.data);
+    
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+    {JSON.stringify(state)}
     </div>
   );
 }
