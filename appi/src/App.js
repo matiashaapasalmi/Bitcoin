b6f70aa7bc3baf50f543ffde1a55e6fab921c73a
Matias <matias.haapasalmi@live.com>
2021-11-28 23:10:54 +0200

    input boxit saatu aikaan

--- appi/src/App.js
+++ appi/src/App.js
@@ -4,6 +4,14 @@ const axios = require("axios").default;
 
 let url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur";
 
+let action;
+
+const tekstityyli = {
+    
+  padding: "300px 300px 300px 600px",
+  justifyContent: "center",
+  alignItems: "center",
+}
 
 function App() {
 
@@ -17,9 +25,27 @@ function App() {
     })
   }, [])
 
+
+  function getData(val)
+  {
+    console.warn(val.target.value)
+  }
+
   return (
 
-    <div>
+    <div style={tekstityyli}>
+      Set range from start to finish in (ddmmyyyy)
+      <div>
+      <label>Start   </label> 
+      <input type="text" onChange={getData}  />
+      <label>Finish   </label>
+      <input type="text" onChange={getData}  />
+      </div>
+     
+      
+      
+
+
     {JSON.stringify(state)}
     </div>
   );
